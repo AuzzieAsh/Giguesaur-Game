97f2657a62141f1bf59ae5e80b45958beed6280b
Ashley Manson <ashley.manson@hotmail.com>
2015-06-10 15:05:03 +1200

    Oh god, why

---
+++ iOS/Giguesaur/Giguesaur/Piece.h
@@ -0,0 +1,36 @@
+/*
+ File: Piece.h
+ Author: Ashley Manson
+ 
+ Description: Header File for what a piece is.
+ */
+
+#ifndef Giguesaur_Piece_h
+#define Giguesaur_Piece_h
+
+@interface PieceClass : NSObject
+
+struct Piece {
+    int piece_id;
+    double x_location;
+    double y_location;
+    double rotation;
+    
+    struct Edge_piece {
+        int up_piece;
+        int down_piece;
+        int left_piece;
+        int right_piece;
+    };
+    
+    struct Edge_open {
+        BOOL up_open;
+        BOOL down_open;
+        BOOL left_open;
+        BOOL right_open;
+    };
+};
+
+@end
+
+#endif
---
+++ iOS/Giguesaur/Giguesaur/Puzzle.h
@@ -0,0 +1,12 @@
+/*
+ File: Puzzle.h
+ Author: Ashley Manson
+ 
+ Description: Header File for what a puzzle is.
+ */
+
+#ifndef Giguesaur_Puzzle_h
+#define Giguesaur_Puzzle_h
+
+
+#endif
