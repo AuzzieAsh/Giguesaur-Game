37270fa058a6970f8c02e0a30953e7b1b4e24783
Ashley Manson <ashley.manson@hotmail.com>
2015-09-24 18:02:43 +1200

    almost 2000 word

--- Written Resources/final report/make.sh
+++ Written Resources/final report/make.sh
@@ -3,3 +3,4 @@ pdflatex AshleyMansonFinalReport.tex
 bibtex AshleyMansonFinalReport.aux
 pdflatex AshleyMansonFinalReport.tex
 pdflatex AshleyMansonFinalReport.tex
+detex AshleyMansonFinalReport.tex | wc -w
