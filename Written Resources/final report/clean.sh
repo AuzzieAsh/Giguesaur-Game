0c5dfe0ba47477043f0d0bfb8b455b255d2f6d41
Ashley Manson <ashley.manson@hotmail.com>
2015-09-30 18:33:28 +1300

    made a Makefile, cause procrastination

--- Written Resources/final report/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -f *.aux
-rm -f *.bbl
-rm -f *.blg
-rm -f *.log
-rm -f *.out
-rm -f *.toc
-rm -f *~
-rm -f .DS_Store
--- Written Resources/final report/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pdflatex AshleyMansonFinalReport.tex
-bibtex AshleyMansonFinalReport.aux
-pdflatex AshleyMansonFinalReport.tex
-pdflatex AshleyMansonFinalReport.tex
-detex AshleyMansonFinalReport.tex | wc -w
